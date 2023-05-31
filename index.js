bd8b42050b50eddf6607ced4002b8f9040d32934
mtegarps <mtegarps@gmail.com>
2023-05-31 10:03:52 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const app = express()
+const routes = require('./routes')
+const { ProfileRepositories } = require('./repositories')
+
+app.use(cors())
+app.use(bodyParser.json({limit: '200kb'}))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(routes)
+app.use(express.json())
+
+app.listen(3001)
